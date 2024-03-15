ea7907accbf186bc405a65c80a09284e932e71ad
MinotourDmitriy <86320101+MinotourDmitriy@users.noreply.github.com>
2024-03-15 18:25:44 +0300

    add_sots_economic_files

--- database/seeders/PageArrays/sots-ekonom.php
+++ database/seeders/PageArrays/sots-ekonom.php
@@ -3,7 +3,7 @@
 $sots_ec = [
     'Социально-экономическое развитие Курской области' => [
         "menu_marker" => "Социально-экономическое развитие",
-        "text" => "sots_econom_razv.html",
+        "text" => "RUS_sots_econom_razv.html",
         "img" => "social_qoute_starovoit.webp",
         "banner" => "centre.webp",
         "files" => [
@@ -27,90 +27,205 @@ $sots_ec = [
 
         "subpage" => [
             "Промышленное и обрабатывающие производства" => [
-                "text" => "sots_econom_razv__prom_obr_p.html",
+                "text" => "RUS_sots_econom_razv__prom_obr_p.html",
                 "img" => "",
-                "banner" => "centre.webp",
+                "banner" => "factory.webp",
 
                 "inner_img" => [
                     [
                         "title" => "",
-                        "file" => "pishhevka-5.jpg"
+                        "file" => "pishhevka-1.webp"
                     ],
                     [
                         "title" => "",
-                        "file" => "pishhevka-4.jpg"
+                        "file" => "pishhevka-2.webp"
                     ],
                     [
                         "title" => "",
-                        "file" => "pishhevka-2.jpg"
+                        "file" => "pishhevka-3.webp"
                     ],
                     [
                         "title" => "",
-                        "file" => "pishhevka-2-1.jpg"
+                        "file" => "pishhevka-4.webp"
                     ],
                     [
                         "title" => "",
-                        "file" => "pishhevka-1.jpg"
+                        "file" => "pishhevka-5.webp"
                     ],
                 ]
             ],
             "Сельское хозяйство" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__agriculture.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "crop.webp",
+
+                "inner_img" => [
+                    [
+                        "title" => "",
+                        "file" => "selhoz1.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "selhoz2.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "selhoz3.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "selhoz4.webp"
+                    ],
+                ]
             ],
             "Инвестиции" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__investments.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "analitics.webp",
+
+                "inner_img" => [
+                    [
+                        "title" => "",
+                        "file" => "Invest1.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest2.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest3.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest4.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest5.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest6.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest7.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest8.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Invest9.webp"
+                    ],
+                ]
             ],
 
             "ЖКХ и благоустройство" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__zkh_landscaping.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "sunset.webp",
+
+                "inner_img" => [
+                    [
+                        "title" => "",
+                        "file" => "ZKHBlago1.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "ZKHBlago2.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "ZKHBlago3.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "ZKHBlago4.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "ZKHBlago5.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "ZKHBlago6.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "ZKHBlago7.webp"
+                    ],
+                ]
             ],
 
             "Малое и среднее предпринимательство" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__small_medium_enterprises.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "asi.webp"
             ],
 
             "Уровень жизни населения" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__standards_living.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "areas.webp"
             ],
 
             "Труд и занятость" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__labor_employment.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "industry.webp"
             ],
 
             "Добыча полезных ископаемых" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__mining.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "margin.webp",
+
+                "inner_img" => [
+                    [
+                        "title" => "",
+                        "file" => "Dobichaiskop1.webp"
+                    ],
+                ]
             ],
 
             "Обеспечение электрической энергией, газом и паром, а также кондиционирование воздуха" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__providing_el_gas_air.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "town.webp"
             ],
 
             "Транспортная инфраструктура" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__transport_infrastructure.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "road.webp"
             ],
 
             "Строительство" => [
-                "text" => "presentattion.html",
+                "text" => "RUS_sots_econom_razv__construct.html",
                 "img" => "",
-                "banner" => "centre.webp"
+                "banner" => "construction.webp",
+
+                "inner_img" => [
+                    [
+                        "title" => "",
+                        "file" => "Stroitelstvo1.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Stroitelstvo2.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Stroitelstvo3.webp"
+                    ],
+                    [
+                        "title" => "",
+                        "file" => "Stroitelstvo4.webp"
+                    ],
+                ]
             ],
         ]
     ],
