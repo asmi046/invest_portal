4e3e1650460507e037c1aa6a02e7e61f600ae32f
asmi046 <asmi046@gmail.com>
2024-03-16 23:23:31 +0300

    +1

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,13 +7,35 @@ use App\Models\PolResurs;
 use Illuminate\Http\Request;
 use App\Actions\MenuStructAction;
 
-class MunicipalController extends Controller
+class IndexController extends Controller
 {
     public function index() {
-
+        $main_news = News::orderBy('public_time', "DESC")->take(6)->get();
+        $resurces = PolResurs::all();
+        return view('index', [
+            'main_news' => $main_news,
+            'resurces' => $resurces
+        ]);
+    }
+    public function exemple() {
+        return view('exemple');
     }
 
-    public function page() {
+    public function test() {
+        $menusAction = new MenuStructAction();
+        $menus = $menusAction->handle();
+    }
 
+    public function socioEconomicDevelopment() {
+        return view('socioEconomicDevelopment');
+    }
+    public function about_gchp() {
+        return view('about_gchp');
+    }
+    public function presentationRegion() {
+        return view('presentationRegion');
+    }
+    public function industrial_sites() {
+        return view('industrial_sites');
     }
 }
--- app/Http/Controllers/MunicipalController.php
+++ app/Http/Controllers/MunicipalController.php
@@ -7,35 +7,11 @@ use App\Models\PolResurs;
 use Illuminate\Http\Request;
 use App\Actions\MenuStructAction;
 
-class IndexController extends Controller
+class MunicipalController extends Controller
 {
     public function index() {
-        $main_news = News::orderBy('public_time', "DESC")->take(6)->get();
-        $resurces = PolResurs::all();
-        return view('index', [
-            'main_news' => $main_news,
-            'resurces' => $resurces
-        ]);
     }
-    public function exemple() {
-        return view('exemple');
-    }
+    public function page() {
 
-    public function test() {
-        $menusAction = new MenuStructAction();
-        $menus = $menusAction->handle();
-    }
-
-    public function socioEconomicDevelopment() {
-        return view('socioEconomicDevelopment');
-    }
-    public function about_gchp() {
-        return view('about_gchp');
-    }
-    public function presentationRegion() {
-        return view('presentationRegion');
-    }
-    public function industrial_sites() {
-        return view('industrial_sites');
     }
 }
