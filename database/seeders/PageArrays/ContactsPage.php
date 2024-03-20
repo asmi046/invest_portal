91d9d9b0f38e494473bf473624d2d059b3855379
asmi046 <asmi046@gmail.com>
2024-03-20 13:21:51 +0300

    Линия прямых обращений

---
+++ database/seeders/PageArrays/ContactsPage.php
@@ -0,0 +1,11 @@
+<?php
+$contacts_page = [
+    'Линия прямых обращений' => [
+        "menu_marker" => "Линия прямых обращений",
+        'title_en' => 'The line of direct appeals',
+        "text" => "RUS_contacts_line.html",
+        "text_en" => "ENG_contacts_line.html",
+        "img" => "",
+        "banner" => "contacts.webp",
+    ]
+];
