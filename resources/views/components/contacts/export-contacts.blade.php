51ba5d3622c7f70273444f572efc2d42f266c748
asmi046 <asmi046@gmail.com>
2024-03-26 10:32:10 +0300

    +1

--- resources/views/components/contacts/export-contacts.blade.php
+++ resources/views/components/contacts/export-contacts.blade.php
@@ -30,26 +30,4 @@
 
     @endforeach
 
-    {{-- <div class="irtse-item">
-        <img src="{{asset('img/tmp/irtse-item-2.webp')}}" alt="" class="irtse-item__img">
-        <ul>
-            <li>305000, г. Курск, ул. Горького, д. 34</li>
-            <li>+7 (4712) 70-33-77,  8 -800-201-33-77</li>
-            <li>info@cpp46.ru, cpp46@mail.ru</li>
-        </ul>
-    </div>
-    <div class="irtse-item">
-        <img src="{{asset('img/tmp/irtse-item-3.webp')}}" alt="" class="irtse-item__img">
-        <ul>
-            <li>Для просмотра аналитического портала «Экспорт регионов России», необходима регистрация на сайте АО «РЭЦ»</li>
-            <li><a href="www.exportcenter.ru">www.exportcenter.ru</a></li>
-        </ul>
-    </div>
-    <div class="irtse-item">
-        <img src="{{asset('img/tmp/irtse-item-4.webp')}}" alt="" class="irtse-item__img">
-
-        <ul>
-            <li><b>«Мой экспорт»</b>  – онлайн доступ к государственным и бизнес сервисам, сопровождающим выход компаний на экспорт, в режиме «Одно окно»</li>
-        </ul>
-    </div> --}}
 </div>
