e28779002fc3e27d970e955babf3597e4dcb07e0
asmi046 <asmi046@gmail.com>
2024-03-26 10:31:25 +0300

    Контакты експортеров

--- app/View/Components/Contacts/ExportContacts.php
+++ app/View/Components/Contacts/ExportContacts.php
@@ -3,17 +3,26 @@
 namespace App\View\Components\Contacts;
 
 use Closure;
-use Illuminate\Contracts\View\View;
+use App\Models\Contact;
 use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
 
 class ExportContacts extends Component
 {
+
+    public $export_contacts = [];
+
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
-        //
+        $this->export_contacts = Contact::where('selector', 'Поддержка экспортеров')->get();
+
+        // dd($export_contacts);
+        // foreach ($ex_contacts as $item)
+        //     $export_contacts[$item->organ] = $item;
     }
 
     /**
--- resources/views/components/contacts/export-contacts.blade.php
+++ resources/views/components/contacts/export-contacts.blade.php
@@ -1,14 +1,36 @@
 <h2>{{__('Информационные ресурсы поддержки экспортеров')}}</h2>
 <div class="irtse-item-box">
-    <div class="irtse-item">
-        <img src="{{asset('img/tmp/irtse-item-1.webp')}}" alt="" class="irtse-item__img">
-        <ul>
-            <li>123610, Россия, г. Москва Краснопресненская набережная, дом 12, подъезд 9</li>
-            <li>+7 (495) 937-4747, 8-800-550-01-88</li>
-            <li><a href="info@exportcenter.ru">info@exportcenter.ru</a></li>
-        </ul>
-    </div>
-    <div class="irtse-item">
+    @foreach ($export_contacts as $item)
+        <div class="irtse-item">
+            @if ($item->img)
+                <img src="{{ Storage::url('contacts/'.$item->img) }}" alt="{{ $item->title }}" class="irtse-item__img">
+            @else
+                <img src="{{asset('img/nophoto.jpg')}}" alt="{{ $item->title }}">
+            @endif
+
+            <ul>
+                @if ($item->organ)
+                    <li>{{_tr($item->organ,$item->organ_en)}}</li>
+                @endif
+
+                @if ($item->adres)
+                    <li>{{_tr($item->adres, $item->adres_en)}}</li>
+                @endif
+
+                @if ($item->phone)
+                    <li><a href="tel:+7{{phone_format($item->phone)}}">{{ $item->phone }}</a></li>
+                @endif
+
+                @if ($item->email)
+                    <li><a href="mailto:{{ $item->email }}">{{ $item->email }}</a></li>
+                @endif
+
+            </ul>
+        </div>
+
+    @endforeach
+
+    {{-- <div class="irtse-item">
         <img src="{{asset('img/tmp/irtse-item-2.webp')}}" alt="" class="irtse-item__img">
         <ul>
             <li>305000, г. Курск, ул. Горького, д. 34</li>
@@ -25,8 +47,9 @@
     </div>
     <div class="irtse-item">
         <img src="{{asset('img/tmp/irtse-item-4.webp')}}" alt="" class="irtse-item__img">
+
         <ul>
             <li><b>«Мой экспорт»</b>  – онлайн доступ к государственным и бизнес сервисам, сопровождающим выход компаний на экспорт, в режиме «Одно окно»</li>
         </ul>
-    </div>
+    </div> --}}
 </div>
